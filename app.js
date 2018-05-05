1d5ca31ef55b59e3471d1da3df3047b7de9f5f80
Jaime González <jgonzalez1@uc.cl>
2018-05-05 19:16:42 -0300

    Linter fix

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const nodemailer=require('nodemailer');
 let app= express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
-app.use(express.static(path.join(__dirname,"public")))
+app.use(express.static(path.join(__dirname,'public')));
 
 app.listen(process.env.PORT || 3000);
 console.log('Server is running on port 3000');
@@ -22,39 +22,39 @@ app.get('/', function (req,res) {
 });
 
 app.get('/about', function(req, res){
-    res.render('about');
+	res.render('about');
 });
 
 app.get('/contact', function(req, res){
-    res.render('contact');
+	res.render('contact');
 });
 
-app.post("/contact/send",function (req,res) {
-    let transport=nodemailer.createTransport({
-        service:"gmail",
-        auth:{
-            user:"expressmail.jaimego@gmail.com",
-            pass:"alotofletters77-"
-        }
-    })
+app.post('/contact/send',function (req,res) {
+	let transport=nodemailer.createTransport({
+		service:'gmail',
+		auth:{
+			user:'expressmail.jaimego@gmail.com',
+			pass:'alotofletters77-'
+		}
+	});
 
-    let mailOptions = {
-        from: 'Jaime González <expressmail.jaimego@gmail.com>',
-        to: 'expressmail.jaimego@gmail.com',
-        subject: 'Express Mailer Submission',
-        text: 'You have a submission with the following details: Name: '+req.body.name+'Email: '+req.body.email+'Message: '+req.body.message,
-        html: '<p>You have a submission with the following details:</p><ul><li>'+req.body.name+'</li><li>Email: '+req.body.email+'</li><li>Message: '+req.body.message+'</li></ul>'
-    };
+	let mailOptions = {
+		from: 'Jaime González <expressmail.jaimego@gmail.com>',
+		to: 'expressmail.jaimego@gmail.com',
+		subject: 'Express Mailer Submission',
+		text: 'You have a submission with the following details: Name: '+req.body.name+'Email: '+req.body.email+'Message: '+req.body.message,
+		html: '<p>You have a submission with the following details:</p><ul><li>'+req.body.name+'</li><li>Email: '+req.body.email+'</li><li>Message: '+req.body.message+'</li></ul>'
+	};
 
-    transport.sendMail(mailOptions, function(error, info){
-        if(error){
-            console.log(error);
-            res.redirect('/');
-        } else {
-            console.log('Message Sent: '+info.response);
-            res.redirect('/');
-        }
-    });
+	transport.sendMail(mailOptions, function(error, info){
+		if(error){
+			console.log(error);
+			res.redirect('/');
+		} else {
+			console.log('Message Sent: '+info.response);
+			res.redirect('/');
+		}
+	});
 
-})
+});
 
